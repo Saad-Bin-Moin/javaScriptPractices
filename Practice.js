2134c91a422d80ca46889a50adf4aac11d72c144
Saad-Bin-Moin <saad200395@gmail.com>
2024-08-22 19:23:42 +0600

    JavaScript Practice Project

---
+++ Practice.js
@@ -0,0 +1,11 @@
+let x = 5;
+let y = 2;
+let z = 6;
+let p =10;
+let q=20;
+let a = "hello " + "saad" + x + y + z + p +q; 
+
+console.log(a);
+// console.log(x);
+// console.log(y);
+// console.log(z);
